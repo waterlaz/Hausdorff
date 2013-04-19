dcdb2552aa2af6350e690033dc92470548d99ac1
waterlaz <waterlaz@gmail.com>
2013-04-19 19:10:13 +0300

    added header files

---
+++ src/contour.h
@@ -0,0 +1,24 @@
+#ifndef __CONTOUR_H_
+#define __CONTOUR_H_ 
+
+#include "image.h"
+
+typedef struct _point_t {
+    double x, y;
+} point_t;
+
+typedef struct _contour_t {
+    int n;
+    point_t* points;
+} contour_t;
+
+typedef struct _contour_set_t {
+    contour_t* root;
+    int n;  /* number of contours inside the current contours */
+    contour_t* children;
+} contour_t;
+
+contour_set_t* find_contours(image_t* img);
+
+
+#endif
---
+++ src/image.h
@@ -0,0 +1,14 @@
+#ifndef __IMAGE_H_
+#define __IMAGE_H_ 
+
+typedef struct _pixel_t {
+    int r, g, b;
+} pixel_t;
+
+typedef struct _image_t {
+    int w;
+    int h;
+    pixel_t** img;
+}
+
+#endif

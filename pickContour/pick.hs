2c512c58b37b4fbc3730b20800f4bec5be218c52
waterlaz <waterlaz@gmail.com>
2014-04-09 17:40:56 +0300

    more progress

--- pickContour/pick.hs
+++ pickContour/pick.hs
@@ -16,7 +16,8 @@ import Data.Maybe
 
 main = do
   args <- getArgs
-  viewer $ head args
+  let (pathImg:pathCont:_) = args
+  viewer pathImg pathCont
 
 type Contour = [(Int, Int)]
 
@@ -25,7 +26,19 @@ data ContourTrea = ContourTrea {
                     subTrea :: [ContourTrea]
                 }
 
-viewer path = do
+readContour :: String -> IO Contour
+readContour s = do
+    f <- readFile s
+    let (nStr:pointsStr) = lines f
+        n = read nStr :: Int
+        pointFromStr str = (round $ read s1, round $ read s2)
+                    where (s1:s2:_) = words str
+    return $ map pointFromStr pointsStr
+
+drawContour pm gc c = 
+    zipWithM (drawLine pm gc)  c (tail $ c++[head c])
+
+viewer pathImg pathCont = do
   initGUI
   win <- windowNew
   win `onDestroy` mainQuit
@@ -37,7 +50,7 @@ viewer path = do
   scrollWin1 <- imageScrollWinNew view
   win `containerAdd` scrollWin1
   
-  pixbuf <- pixbufNewFromFile path
+  pixbuf <- pixbufNewFromFile pathImg
   w <- pixbufGetWidth pixbuf
   h <- pixbufGetHeight pixbuf
   pm <- pixmapNew (Nothing :: Maybe Pixmap) w h (Just 24)
@@ -66,13 +79,15 @@ viewer path = do
                         drawPixbuf pm gc pixbuf 0 0 0 0 (-1) (-1) RgbDitherNone 0 0
                         gcSetValues gc newGCValues{ foreground = Color 65535 0 0 }
                         drawLine pm gc (0, 0) (w, h)
+                        cont <- readContour pathCont
+                        drawContour pm gc cont
                         pixbuf2 <- liftM fromJust $ pixbufGetFromDrawable pm (Rectangle 0 0 (w-1) (h-1))
                         imageViewSetPixbuf view (Just pixbuf2) True
                         set win [ windowTitle := img ])
               (\_ -> print "error")
 
 
-  setImage path
+  setImage pathImg
 
 
   widgetShowAll win
